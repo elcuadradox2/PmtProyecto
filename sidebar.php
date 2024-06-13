e552d0712adbbc3f68d820c19fced120254e622b
elcuadradox2 <jlemusr11@miumg.edu.gt>
2024-06-12 22:26:36 -0600

    Cambio navbar removido de bootstrap sidebar

--- sidebar.php
+++ sidebar.php
@@ -285,6 +285,14 @@ if($position=='digitalizador') {
 	 <div class="main-panel">
 		<nav class="navbar navbar-default navbar-fixed">
             <div class="container-fluid">
+            <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                  </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li>
