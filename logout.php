fed9ab573e6c714a56b1015759e1b6bb099c28c4
elcuadradox2 <jlemusr11@miumg.edu.gt>
2024-08-16 22:34:06 -0600

    Cambios para proyecto final

--- logout.php
+++ logout.php
@@ -1,14 +1,6 @@
 <?php
-/**................................................................
- * @package eblog v 1.0
- * @author Faith Awolu 
- * Hillsofts Technology Ltd.            
- * (hillsofts@gmail.com)
- * ................................................................
- */
-
-
 session_start();
 session_destroy();
 header("location: login.php");
+exit();
 ?>
